8565c97d0294364da46750493af67125b4d701ff
connor hoehn <connorhoehn@gmail.com>
2025-08-01 12:22:17 -0400

    Update node discovery / seed.

---
+++ src/cluster/seeding/index.ts
@@ -0,0 +1,8 @@
+export { SeedNodeInfo, SeedNodeHealthStatus, SeedNodeRegistry } from './SeedNodeRegistry';
+export { 
+  SeedDiscoveryStrategy, 
+  SeedDiscoveryConfig, 
+  DiscoveryResult, 
+  SeedNodeDiscovery 
+} from './SeedNodeDiscovery';
+export { EnhancedBootstrapOptions, EnhancedBootstrapConfig } from './EnhancedBootstrapConfig';
